2bc87b66955ac16ac2aad3b279a03997ccc9d03c
sampathdissanayaka <dmcsampath97@gmail.com>
2020-12-10 20:31:13 +0700

    add tabbar & tabbar view

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 void main() {
   runApp(MyApp());
 }
@@ -24,28 +23,52 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar:AppBar(
-        title: Text("Chamara Sampath"),
-        leading: IconButton(
-          icon: Icon(Icons.menu),
-          onPressed: (){},
-        ),
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        appBar:AppBar(
+          title: Text("Chamara Sampath"),
+          leading: IconButton(
+            icon: Icon(Icons.menu),
+            onPressed: (){},
+          ),
 
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(Icons.search),
-            onPressed: (){},
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.search),
+              onPressed: (){},
+            ),
+            IconButton(
+              icon: Icon(Icons.more_vert),
+              onPressed: (){},
+            ),
+          ],
+          flexibleSpace: Image.asset("assets/ab.jpg",
+            fit: BoxFit.cover,),
+          bottom: TabBar(
+            tabs: [
+              Tab(icon: Icon(Icons.directions_car)),
+              Tab(icon: Icon(Icons.directions_transit)),
+              Tab(icon: Icon(Icons.directions_bike)),
+            ],
           ),
-          IconButton(
-            icon: Icon(Icons.more_vert),
-            onPressed: (){},
-          ),
-        ],
-        flexibleSpace: Image.asset("assets/ab.jpg",
-          fit: BoxFit.cover,),
+        ),
+        body: TabBarView(
+          children: [
+            tab1(),
+            Icon(Icons.directions_transit),
+            Icon(Icons.directions_bike),
+          ],
+        ),
       ),
     );
   }
 }
 
+Widget tab1() {
+  return Container(
+    child: Center(
+      child: Text("Tab01"),
+    ),
+  );
+}
